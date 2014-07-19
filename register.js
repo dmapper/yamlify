c1f287287f768cd7626aed959c6f0eb2d2b4b514
Pavel Zhukov <cray0000@gmail.com>
2014-07-19 23:54:19 +0200

    Prevent possible other modules that depend on js-yaml from changing this one's yaml require behavior (js-yaml 3.0.2 has its own yaml require handlers for node.js)

--- register.js
+++ register.js
@@ -2,10 +2,19 @@ var fs   = require('fs');
 var yaml = require('js-yaml');
 var includeSchema = require('./includeSchema');
 
-require.extensions['.yml'] = require.extensions['.yaml'] = function (m, f) {
+var yamlRequire = function (m, f) {
   m.exports = yaml.safeLoad(fs.readFileSync(f, 'utf8'), {
     schema: includeSchema,
     filename: f
   });
 };
 
+// Hack to prevent later changes of .yml, .yaml extensions handlers
+['.yml', '.yaml'].forEach(function(ext){
+  Object.defineProperty(require.extensions, ext, {
+    get: function() {
+      return yamlRequire;
+    },
+    set: function() {}
+  });
+});
